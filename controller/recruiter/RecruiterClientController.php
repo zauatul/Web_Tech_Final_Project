93df8b571be8e889814234fa20a9fd191181dd13
zauatu <mdsk6768@gmail.com>
2026-05-17 18:44:55 +0600

    RecruiterClientController file added

---
+++ controller/recruiter/RecruiterClientController.php
@@ -0,0 +1,48 @@
+<?php
+
+
+include_once __DIR__ . "/../../model/RecruiterModel.php";
+
+$companyNameOverride = "";
+
+$employerErr = "";
+$success = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $recruiterId = $_SESSION["user_id"];
+
+    if(empty($_POST["employer_id"]))
+    {
+        $employerErr = "Employer is required";
+    }
+    else
+    {
+        $employerId = $_POST["employer_id"];
+    }
+
+    if(isset($_POST["company_name_override"]))
+    {
+        $companyNameOverride = trim($_POST["company_name_override"]);
+    }
+
+    if($employerErr == "")
+    {
+        $ok = addClient(
+            $recruiterId,
+            $employerId,
+            $companyNameOverride
+        );
+
+        if($ok)
+        {
+            $success = "Client added successfully";
+        }
+        else
+        {
+            $employerErr = "Could not add client";
+        }
+    }
+}
+
+?>
