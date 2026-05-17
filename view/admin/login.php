b89eaa7fe2dc9b405b92299b38d74658df4a2c1a
arnob202 <arnobab4@gmail.com>
2026-05-17 16:56:34 +0600

    Added admin login module

---
+++ view/admin/login.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>
+Admin Login
+</title>
+
+<link rel="stylesheet"
+href="../../assets/css/style.css">
+
+<script>
+
+function validateForm()
+{
+
+var email =
+document.getElementById("email").value;
+
+var password =
+document.getElementById("password").value;
+
+if(email == "")
+{
+   alert("Email Required");
+   return false;
+}
+
+if(password == "")
+{
+   alert("Password Required");
+   return false;
+}
+
+return true;
+
+}
+
+</script>
+
+</head>
+
+<body>
+
+<div class="container">
+
+<h2>
+Admin Login
+</h2>
+
+<form
+action="../../controller/admin/AdminLoginController.php"
+method="POST"
+onsubmit="return validateForm()"
+>
+
+<input type="text"
+name="email"
+id="email"
+placeholder="Enter Email">
+
+<input type="password"
+name="password"
+id="password"
+placeholder="Enter Password">
+
+<input
+type="submit"
+name="login"
+value="Login">
+
+</form>
+
+<p class="hint">
+<a href="../../index.php">Portal home</a>
+</p>
+
+</div>
+
+</body>
+</html>
