e8e74299f21ceccda1a3a06ecbbbbcb6885d9e86
arnob202 <arnobab4@gmail.com>
2026-05-17 16:36:48 +0600

    Added database conectione

---
+++ config/db.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "job_portal";
+
+$conn = new mysqli(
+$host,
+$user,
+$password,
+$database
+);
+
+if($conn->connect_error)
+{
+    die("Connection Failed");
+}
+
+?>
