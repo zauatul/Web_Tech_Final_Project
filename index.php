2f3d43f14fd313a4389defcad6955c3ca68adb26
arnob202 <arnobab4@gmail.com>
2026-05-18 23:42:25 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="utf-8">
+<title>
+Job portal
+</title>
+
+<link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+
+<div class="container employer-auth">
+
+<h2>
+Job portal
+</h2>
+ 
+
+<ul class="dashboard-links">
+
+<li>
+<a href="view/employer/register.php">
+Employer registration
+</a>
+</li>
+
+<li>
+<a href="view/employer/login.php">
+Employer login
+</a>
+</li>
+
+<li>
+<a href="view/seeker/register.php">
+Job seeker registration
+</a>
+</li>
+
+<li>
+<a href="view/seeker/login.php">
+Job seeker login
+</a>
+</li>
+
+<li>
+<a href="view/recruiter/register.php">
+Recruiter registration
+</a>
+</li>
+
+<li>
+<a href="view/recruiter/login.php">
+Recruiter login
+</a>
+</li>
+
+<li>
+<a href="view/admin/login.php">
+Admin login
+</a>
+</li>
+
+</ul>
+
+</div>
+
+</body>
+</html>
