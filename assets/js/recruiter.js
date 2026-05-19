70f03c6adc7a1a3b045ff72983b262756d34fea0
zauatul <143562413+zauatul@users.noreply.github.com>
2026-05-19 10:20:10 +0600

    Refactor recruiterSearchSeekers function updated

--- assets/js/recruiter.js
+++ assets/js/recruiter.js
@@ -1,76 +1,51 @@
 function recruiterSearchSeekers()
 {
+    var keyword = document.getElementById("keyword").value;
 
-var keyword = document.getElementById("keyword").value;
-var xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status == 200){
+            var data = JSON.parse(xhr.responseText);
+            var box = document.getElementById("search-results");
+            var html = "";
+             html += "<table class='employer-table'>";
+                html += "<tr>";
+                html += "<th>Name</th>";
+                html += "<th>Headline</th>";
+                html += "<th>Skills</th>";
+                html += "<th>Experience</th>";
+                html += "<th>Action</th>";
+                html += "</tr>";
+                for(var i = 0; i < data.rows.length; i++)
+                {
+                    html += "<tr>";
+                    html += "<td>" + data.rows[i].name + "</td>";
+                    html += "<td>" + data.rows[i].headline + "</td>";
+                    html += "<td>" + data.rows[i].skills + "</td>";
+                    html += "<td>" + data.rows[i].years_experience + "</td>";
+                    html += "<td>";
+                    html += "<a href='seeker_profile.php?id=" + data.rows[i].id + "'>";
+                    html += "View";
+                    html += "</a>";
+                    html += "</td>";
+                    html += "</tr>";}
+                html += "</table>";
+            box.innerHTML = html;
+        }
+    };
+    xhr.open(
+        "POST",
+        "../../controller/recruiter/RecruiterAjaxController.php",
+        true
+    );
 
-xhr.open("POST", "../../controller/recruiter/RecruiterAjaxController.php", true);
-xhr.setRequestHeader(
-"Content-type",
-"application/x-www-form-urlencoded"
-);
+    xhr.setRequestHeader(
+        "Content-type",
+        "application/x-www-form-urlencoded"
+    );
 
-xhr.onreadystatechange = function()
-{
-
-if(xhr.readyState !== 4)
-{
-    return;
-}
-
-try
-{
-
-    var data = JSON.parse(xhr.responseText);
-    var box = document.getElementById("search-results");
-    var html = "";
-
-    if(!data.ok)
-    {
-        alert(data.error || "Search failed.");
-        return;
-    }
-
-    html += "<table class=\"employer-table\">";
-    html += "<tr><th>Name</th><th>Headline</th><th>Skills</th><th>Experience</th><th></th></tr>";
-
-    for(var i = 0; i < data.rows.length; i++)
-    {
-        html += "<tr>";
-        html += "<td>" + escapeHtml(data.rows[i].name || "") + "</td>";
-        html += "<td>" + escapeHtml(data.rows[i].headline || "") + "</td>";
-        html += "<td>" + escapeHtml(data.rows[i].skills || "") + "</td>";
-        html += "<td>" + escapeHtml(data.rows[i].years_experience || "0") + "</td>";
-        html += "<td><a href=\"seeker_profile.php?id=" + encodeURIComponent(data.rows[i].id) + "\">View</a></td>";
-        html += "</tr>";
-    }
-
-    html += "</table>";
-    box.innerHTML = html;
-
-}
-catch(e)
-{
-    alert("Server error.");
-}
-
-};
-
-xhr.send(
-"action=search_seekers"
-+ "&keyword=" + encodeURIComponent(keyword)
-);
-
-}
-
-function escapeHtml(text)
-{
-
-return String(text)
-.replace(/&/g, "&amp;")
-.replace(/</g, "&lt;")
-.replace(/>/g, "&gt;")
-.replace(/"/g, "&quot;")
-.replace(/'/g, "&#039;");
-
+    xhr.send(
+        "action=search_seekers&keyword="
+        + encodeURIComponent(keyword)
+    );
 }
